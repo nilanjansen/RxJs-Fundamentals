f7632b25dbf1fc1ce6b2adba9c0d354d12694d81
Nilanjan <epigenesis1992@gmail.com>
2019-04-28 19:03:08 +0530

    RxJs Fundamentals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+
+import { take,map } from 'rxjs/operators';
+import { mergeMap,tap,switchMap,debounceTime,distinctUntilChanged } from 'rxjs/operators';
+import { Subject,of,fromEvent } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs';
+  searchSubject$=new Subject<string>();
+  results$:Observable<any>;
+  /**
+   *
+   */
+  constructor(private http:HttpClient) {
+  }
+
+  ngOnInit() { 
+    this.results$ = this.searchSubject$
+      .pipe(debounceTime(200))
+      .pipe(distinctUntilChanged())
+      .pipe(tap(x => console.log('do', x)))
+      .pipe(switchMap(searchString => this.queryAPI(searchString)));
+
+  }
+  ngOnDestroy(){
+    
+  }
+  queryAPI(searchString){
+    console.log('queryAPI',searchString);
+    return this.http.get(`https://www.reddit.com/r/aww/search.json?q=${searchString}`)
+    .pipe(map(result=>result['data']['children']));
+  }
+  inputChanged($event){
+    this.searchSubject$.next($event);
+    console.log('input changed',$event);
+  }
+  
+   
 }
